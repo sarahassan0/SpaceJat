78be7aee6520ac5dd5a5aa8a2ed35f6656093ab0
sarahassan0 <sarraree5@gmail.com>
2024-07-20 18:10:21 +0300

    feat:  implement the subscribers, notification and timeline service.

---
+++ backend/services/notification-service.js
@@ -0,0 +1,23 @@
+// Notification Service
+
+import { connect, StringCodec } from "nats";
+
+(async () => {
+  try {
+    const nc = await connect({ servers: "nats://localhost:4222" });
+    const sc = StringCodec();
+    console.log("Notification Service is running");
+
+    const sub = nc.subscribe("meeting");
+    (async () => {
+      for await (const msg of sub) {
+        const meetingObject = JSON.parse(sc.decode(msg.data));
+        console.log(
+          `You have a new notification update: ${meetingObject["meeting-host"]} is hosting a new meeting. Join now!`
+        );
+      }
+    })();
+  } catch (err) {
+    console.error("Error connecting to NATS:", err);
+  }
+})();
---
+++ backend/services/timeline-service.js
@@ -0,0 +1,37 @@
+//  Timeline Service
+
+import { connect, StringCodec } from "nats";
+
+(async () => {
+  try {
+    const nc = await connect({ servers: "nats://localhost:4222" });
+    const sc = StringCodec();
+
+    console.log("Timeline Service is running");
+
+    const sub = nc.subscribe("meeting");
+    (async () => {
+      for await (const msg of sub) {
+        const meetingObject = JSON.parse(sc.decode(msg.data));
+
+        const now = new Date();
+        const options = {
+          year: "numeric",
+          month: "long",
+          day: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          timeZoneName: "short",
+        };
+        const formattedDate = now.toLocaleString("en-US", options);
+
+        // Posting the new meeting on the timeline.
+        console.log(
+          `${meetingObject["meeting-host"]} is hosting a new meeting on ${formattedDate}. Join now!`
+        );
+      }
+    })();
+  } catch (err) {
+    console.error("Error connecting to NATS:", err);
+  }
+})();
