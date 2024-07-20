65169eeeb331420da155ebe69c5beadddf2284bd
sarahassan0 <sarraree5@gmail.com>
2024-07-20 18:11:50 +0300

    feat:  implement the cloud services, drive and S3 service.

---
+++ backend/services/cloud-services/S3-service.js
@@ -0,0 +1,26 @@
+//  S3 Service
+
+import { connect, StringCodec } from "nats";
+
+(async () => {
+  try {
+    const nc = await connect({ servers: "nats://localhost:4222" });
+    const sc = StringCodec();
+
+    console.log("S3 Service is running");
+
+    const sub = nc.subscribe("recorded_meeting", { queue: "recorded_meeting_queue" });
+    (async () => {
+      for await (const msg of sub) {
+        const meetingObject = JSON.parse(sc.decode(msg.data));
+
+        // "Store the recorded meeting on the S3 Storage."
+        console.log(
+          `S3 Service Update: The meeting hosted by ${meetingObject["meeting-host"]} is being stored on the S3 Storage.`
+        );
+      }
+    })();
+  } catch (err) {
+    console.error("Error connecting to NATS:", err);
+  }
+})();
---
+++ backend/services/cloud-services/drive-service.js
@@ -0,0 +1,26 @@
+//  Drive Service
+
+import { connect, StringCodec } from "nats";
+
+(async () => {
+  try {
+    const nc = await connect({ servers: "nats://localhost:4222" });
+    const sc = StringCodec();
+
+    console.log("Drive Service is running");
+
+    const sub = nc.subscribe("recorded_meeting", { queue: "recorded_meeting_queue" });
+    (async () => {
+      for await (const msg of sub) {
+        const meetingObject = JSON.parse(sc.decode(msg.data));
+
+        // "Store the recorded meeting on the drive Storage."
+        console.log(
+          `Drive Service Update: The meeting hosted by ${meetingObject["meeting-host"]} is being stored on the Drive Storage.`
+        );
+      }
+    })();
+  } catch (err) {
+    console.error("Error connecting to NATS:", err);
+  }
+})();
